2f55535cee50c3827df3830676047871834f82af
Myrddraall <Erd@Dark-Nexus>
2013-04-11 20:43:21 -0400

    regened

--- common/com/myrddraall/AETurtles/mod/ModInfo.java
+++ common/com/myrddraall/AETurtles/mod/ModInfo.java
@@ -6,8 +6,8 @@ public final class ModInfo
 
     public static final String MOD_ID = "AETurtles";
     public static final String MOD_NAME = "AE Turtles";
-    public static final String SERVER = "com.myrddraall.AETurtles.proxy.CommonProxy";
-    public static final String CLIENT = "com.myrddraall.AETurtles.proxy.ClientProxy";
+    public static final String SERVER = "com.myrddraall.AETurtles.mod.proxy.CommonProxy";
+    public static final String CLIENT = "com.myrddraall.AETurtles.mod.proxy.ClientProxy";
     public static final boolean CLIENT_REQUIRED = true;
     public static final boolean SERVER_REQUIRED = false;
 
